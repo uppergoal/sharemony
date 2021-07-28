da504b80205b62e318a491a6ce90f14fbb1ff5ca
= <=>
2021-07-28 09:50:36 -0400

    Added dashboard shell, emptyState and freePlanEmptyState

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import { AuthProvider } from '@/lib/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+import theme from '@/styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
+};
+
+export default App;
